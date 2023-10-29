eae2ba1a62856a28f197191c4be0b8458ce7a3a7
Harun <harungb025@gmail.com>
2023-10-29 17:42:50 +0700

    feat(config): add configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      public: `${path.resolve(__dirname, "./public/")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+      assets: `${path.resolve(__dirname, "./src/assets/")}`,
+      schema: `${path.resolve(__dirname, "./src/schema/")}`,
+    },
+  },
 })
