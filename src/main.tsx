c4e6bf32990485e0ddc6d87c3289be0f3d4df186
Harun <harungb025@gmail.com>
2023-10-29 17:45:40 +0700

    refactor(main): add apollo config and react router config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { router } from './routes'
+import React from 'react'
+import './assets/style.css'
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider 
+} from '@apollo/client'
+import { RouterProvider } from "react-router-dom"
+
+const client = new ApolloClient({
+  uri: 'https://wpe-hiring.tokopedia.net/graphql',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </React.StrictMode>
 )
