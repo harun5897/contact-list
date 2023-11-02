afb2ec246e68a0403e8108b99253805f68546414
Harun <harungb025@gmail.com>
2023-11-02 13:56:49 +0700

    refactor(pages): add pagenition

--- src/pages/PageContact.tsx
+++ src/pages/PageContact.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useQuery } from '@apollo/client'
 import { TypeOfContact } from '@/schema/useContract'
 import { getArrayFromLocalStorage} from '@/schema/useLocalStorage'
+import { useEffect, useState } from 'react'
 import Menu from '@/components/Menu/Menu'
 import Card from '@/components/Card/Card'
 import Avatar from '@/assets/avatar-2.png'
@@ -8,29 +10,64 @@ import Contact from '@/schema/useContact'
 import Container from '@/components/Container/Container'
 
 const ElementProses = () => {
-  const { getContact } = Contact
-  const { loading, error, data } = useQuery(getContact)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error : {error.message}</p>
-  const idFavoriteContact = getArrayFromLocalStorage('favoriteContact')
+    const [contact, setContact] = useState<Array<TypeOfContact>>([])
+    const [offset, setOffset] = useState<number>(0)
+    const [loadings, setLoadings] = useState<boolean>(false)
 
-  return (
-    <>
-      <Menu addContact='Add' myContact='My Contact' favoriteContact='Favorite' />
-      {data.contact.map((detail: TypeOfContact) => {
-        if (!idFavoriteContact.includes(detail.id)) {
-          return (
-            <Card
-              imgSrc={Avatar}
-              key={detail.id}
-              title={detail.first_name}
-              text={detail.phones[0].number}
-              id={detail.id}
-              favorite={false}
-            />
-          )
+    const { getContact } = Contact
+    const { loading, error, data, refetch } = useQuery(getContact, {
+        variables: {
+        offset: 0,
+        limit: 10
         }
-      })}
+    })
+    useEffect(() => {
+        setLoadings(false)
+        if(data && Array.isArray(data.contact)) {
+        setContact(previous => [...previous, ...data.contact])
+    }
+    }, [data])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollHeight = document.documentElement.scrollHeight
+            const currentHeight = window.innerHeight + window.scrollY
+            if(currentHeight + 1 >= scrollHeight && !loadings) {
+                setOffset(prev => prev + 5)
+            }
+        }
+            window.addEventListener('scroll', handleScroll)
+            return () => window.removeEventListener('scroll', handleScroll)
+    },[])
+
+    useEffect(()=> {
+        if(data && data.contact.length > 0) {
+            refetch ({ offset: offset, limit: 10 })
+            setLoadings(true)
+        }
+    },[offset])
+ 
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error : {error.message}</p>
+    const idFavoriteContact = getArrayFromLocalStorage('favoriteContact')
+
+    return (
+    <>
+        <Menu addContact='Add' myContact='My Contact' favoriteContact='Favorite' />
+        {contact.map((detail: TypeOfContact, index: number) => {
+            if (!idFavoriteContact.includes(detail.id)) {
+                return (
+                    <Card
+                        imgSrc={Avatar}
+                        key={index}
+                        title={detail.first_name}
+                        text={detail.phones[0] ? detail.phones[0].number : ''}
+                        id={detail.id}
+                        favorite={false}
+                    />
+                )
+            }
+        })}
     </>
   )
 }
