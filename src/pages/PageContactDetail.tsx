8c42665b2fec1e13e0e82fd2880a4e282f2ac3bd
Harun <harungb025@gmail.com>
2023-11-02 13:57:10 +0700

    refactor(pages): add method delete contact

--- src/pages/PageContactDetail.tsx
+++ src/pages/PageContactDetail.tsx
@@ -1,24 +1,37 @@
-import { useQuery } from '@apollo/client'
-import { useParams } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import { useQuery, useMutation } from '@apollo/client'
+import { useParams, NavLink, useNavigate } from 'react-router-dom'
 import Img from '@/components/Img/Img'
 import Button from '@/components/Button/Button'
 import Avatar from '@/assets/avatar-2.png'
 import Feature from '@/components/Feature/Feature'
 import Contact from '@/components/Contact/Contact'
+import Paragraph from '@/components/Text/TextParagraph'
 import Container from '@/components/Container/Container'
 import MenuDetail from '@/components/Menu/MenuDetail'
 import ContactDetail from '@/schema/useContact'
+import { useState } from 'react'
 
 const ElementContactDetail = () => {
     const { id } = useParams()
-    const { getContactDetail } = ContactDetail
-    const { loading, error, data } = useQuery(getContactDetail, {
-        variables: { "id": id },
-    })
+    const navigate = useNavigate()
+    const [deleteSuccess, setDeleteSuccess] = useState<boolean>(false)
+    const { getContactDetail, deleteContactPhonne } = ContactDetail
+    const [ deleteContactNumber, { error }] = useMutation(deleteContactPhonne)
+    const { loading, data } = useQuery(getContactDetail, { variables: { "id": id } })
+    const deleteContact = async () => {
+        try {
+            const { data } = await deleteContactNumber({variables: { id: id }})
+            if(data) {
+                setDeleteSuccess(true)
+                setTimeout(() => {
+                    navigate('/')
+                    window.location.reload() 
+                }, 1000)
+            }
+        } catch (e) { return false }
+    }
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error : {error.message}</p>
-    
     return (
         <>
             <MenuDetail idContact={data.contact_by_pk.id} /><br/>
@@ -38,6 +51,8 @@ const ElementContactDetail = () => {
             <NavLink to={`/edit-contact/${data.contact_by_pk.id}`}>
                 <Button text='Edit' onClick={()=> {}} />
             </NavLink>
+            <Button text='Delete' onClick={ deleteContact } />
+            {deleteSuccess && <Paragraph text='Delete Contact Success'/> }
         </>
     )
 }
